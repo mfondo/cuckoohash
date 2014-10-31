1541c89760b26f039b7dd0c040fdc1c10089d20b
Mike Friesen <m.fondo@gmail.com>
2014-10-31 18:14:19 -0500

    adding more tests

--- src/main/java/com/github/mfondo/CuckooHashSet.java
+++ src/main/java/com/github/mfondo/CuckooHashSet.java
@@ -1,22 +1,26 @@
 package com.github.mfondo;
 
+import com.google.common.collect.AbstractIterator;
+
+import java.util.AbstractSet;
 import java.util.Collection;
 import java.util.Iterator;
-import java.util.Set;
 
 /**
  * http://en.wikipedia.org/wiki/Cuckoo_hashing
  * Implementation of http://www.cs.tau.ac.il/~shanir/advanced-seminar-data-structures-2007/bib/pagh01cuckoo.pdf
  */
-public class CuckooHashSet<T> implements Set<T> {
+public class CuckooHashSet<T> extends AbstractSet<T> {
+
+    private static final int DEFAULT_INITIAL_SIZE = 8;
 
     private final HashFunction<T> hashFunction1;
     private final HashFunction<T> hashFunction2;
     private final int maxInsertLoops;
     private final float loadFactor;
 
-    private T[] values1;
-    private T[] values2;
+    private Holder<T>[] values1 = new Holder[DEFAULT_INITIAL_SIZE];
+    private Holder<T>[] values2 = new Holder[DEFAULT_INITIAL_SIZE];;
     private int size = 0;
 
     public CuckooHashSet(int maxInsertLoops, float loadFactor, HashFunction<T> hashFunction1, HashFunction<T> hashFunction2) {
@@ -28,7 +32,7 @@ public class CuckooHashSet<T> implements Set<T> {
 
     @Override
     public int size() {
-        throw new UnsupportedOperationException();//todo
+        return size;
     }
 
     @Override
@@ -39,50 +43,113 @@ public class CuckooHashSet<T> implements Set<T> {
     @Override
     public boolean contains(Object o) {
         int hash = hashFunction1.hash((T)o) % values1.length;
-        T t = values1[hash];
+        Holder<T> t = values1[hash];
         if(t == null) {
             hash = hashFunction2.hash((T)o) % values2.length;
             t = values2[hash];
         }
-        return t != null;
+        boolean ret;
+        if(t != null) {
+            ret = t.t.equals(o);
+        } else {
+            ret = false;
+        }
+        return ret;
     }
 
     @Override
     public boolean add(T t) {
-//        T ret = remove(t);
-//        if(ret != null) {
-//            todo replace ret with v
-//        }
-        if(values1 == null || size > (values1.length * loadFactor)) {
-            //resize
+        if(contains(t)) {
+            return false;
         }
-        T ret = null;
+        if(size > (values1.length * loadFactor)) {
+            int newSize = size < 1 ? 16 : size * 2;
+            Holder<T>[] tmp1 = new Holder[newSize];
+            Holder<T>[] tmp2 = new Holder[newSize];
+            addValues(values1, tmp1, tmp2);
+            addValues(values2, tmp1, tmp2);
+            values1 = tmp1;
+            values2 = tmp2;
+        }
+        add(values1, values2, t);
+        size++;
+        return true;
+    }
+
+    private void addValues(Holder<T>[] from, Holder<T>[] tmp1, Holder<T>[] tmp2) {
+        if(from != null) {
+            for(Holder<T> h : from) {
+                if(h != null) {
+                    add(tmp1, tmp2, h.t);
+                }
+            }
+        }
+    }
+
+    private T add(Holder<T>[] values1, Holder<T>[] values2, T t) {
+        Holder<T> ret = null;
         int hash;
         for(int i = 0; i < maxInsertLoops; i++) {
             hash = hashFunction1.hash(t) % values1.length;
             ret = values1[hash];
             if(ret == null) {
-                values1[hash] = t;
+                values1[hash] = new Holder<T>(t);
                 break;
             }
-            values1[hash] = t;
-            t = ret;
+            values1[hash] = new Holder<T>(t);
+            t = ret.t;
             hash = hashFunction2.hash(t) % values2.length;
             ret = values2[hash];
             if(ret == null) {
-                values2[hash] = t;
+                values2[hash] = new Holder<T>(t);
                 break;
             }
-            values2[hash] = t;
-            t = ret;
+            values2[hash] = new Holder<T>(t);
+            t = ret.t;
         }
-        size++;
-        return true;//todo wrong
+        return ret != null ? ret.t : null;
     }
 
     @Override
     public Iterator<T> iterator() {
-        throw new UnsupportedOperationException();//todo
+        return new AbstractIterator<T>() {
+
+            private boolean inValues1 = true;
+            private int currentPos = 0;
+
+            @Override
+            protected T computeNext() {
+                T ret = null;
+                Holder<T> h;
+                if(inValues1) {
+                    for(; currentPos < values1.length; currentPos++) {
+                        h = values1[currentPos];
+                        if(h != null) {
+                            ret = h.t;
+                            break;
+                        }
+                    }
+                    if(currentPos > values1.length - 1) {
+                        inValues1 = false;
+                        currentPos = 0;
+                    }
+                }
+                if(!inValues1) {
+                    for(; currentPos < values2.length; currentPos++) {
+                        h = values2[currentPos];
+                        if(h != null) {
+                            ret = h.t;
+                            break;
+                        }
+                    }
+                }
+                currentPos++;
+                if(ret == null) {
+                    ret = endOfData();
+                }
+                return ret;
+            }
+        };
     }
 
     @Override
@@ -103,7 +170,13 @@ public class CuckooHashSet<T> implements Set<T> {
 
     @Override
     public boolean containsAll(Collection<?> c) {
-        throw new UnsupportedOperationException();//todo
+        int containCnt = 0;
+        for(Object a : c) {
+            if(contains(a)) {
+                containCnt++;
+            }
+        }
+        return containCnt == c.size();
     }
 
     @Override
@@ -130,14 +203,13 @@ public class CuckooHashSet<T> implements Set<T> {
         int hash(K k);
     }
 
-    private boolean containsValue(T[] table, Object o) {
-        boolean ret = false;
-        for(T t : table) {
-            if(t.equals(o)) {
-                ret = true;
-                break;
-            }
+    //todo this makes things much less efficient to have to have the indirect pointer
+    //todo maybe try Array.newInstance(c, s) and pass in the class to constructor?
+    private static class Holder<T> {
+        private T t;
+
+        private Holder(T t) {
+            this.t = t;
         }
-        return ret;
     }
 }
--- src/test/java/com/github/mfondo/CuckooHashSetTest.java
+++ src/test/java/com/github/mfondo/CuckooHashSetTest.java
@@ -2,6 +2,7 @@ package com.github.mfondo;
 
 import junit.framework.TestCase;
 
+import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -13,7 +14,7 @@ import java.util.Set;
 public class CuckooHashSetTest extends TestCase {
 
     public void test1() {
-        Set<Integer> set = new CuckooHashSet<Integer>(100, new CuckooHashSet.HashFunction<Integer>() {
+        final Set<Integer> cuckooSet = new CuckooHashSet<Integer>(100, 0.9f, new CuckooHashSet.HashFunction<Integer>() {
             @Override
             public int hash(Integer integer) {
                 return integer % 7;
@@ -25,7 +26,56 @@ public class CuckooHashSetTest extends TestCase {
             }
         });
 
-        set.add(3);
-        assertTrue(set.contains(3));
+        //todo remove - for debugging
+        /*dumpContents(cuckooSet);
+        cuckooSet.add(1);
+        dumpContents(cuckooSet);
+        cuckooSet.add(2);
+        dumpContents(cuckooSet);
+        for(int i = 3; i < 10; i++) {
+            cuckooSet.add(i);
+        }
+        dumpContents(cuckooSet);*/
+
+        final Set<Integer> hashSet = new HashSet<Integer>();
+
+        assertAdd(cuckooSet, hashSet, 1);
+        assertTrue(cuckooSet.contains(1));
+        assertFalse(cuckooSet.contains(2));
+
+        assertAdd(cuckooSet, hashSet, 2);
+        assertTrue(cuckooSet.contains(1));
+        assertTrue(cuckooSet.contains(2));
+        assertFalse(cuckooSet.contains(3));
+
+        assertAdd(cuckooSet, hashSet, 1);
+        assertRemove(cuckooSet, hashSet, 1);
+
+        for(int i = 0; i < 10; i++) {
+            assertAdd(cuckooSet, hashSet, i);
+        }
+        for(int i = 0; i < 10; i++) {
+            assertRemove(cuckooSet, hashSet, i);
+        }
+    }
+
+    //todo remove - this is for debugging
+    private void dumpContents(Set<Integer> set) {
+        for(Integer i : set) {
+            System.out.print(i + ",");
+        }
+        System.out.println();
+    }
+
+    private void assertAdd(Set<Integer> set1, Set<Integer> set2, int i) {
+        set1.add(i);
+        set2.add(i);
+        assertEquals(set1, set2);
+    }
+
+    private void assertRemove(Set<Integer> set1, Set<Integer> set2, int i) {
+        set1.remove(i);
+        set2.remove(i);
+        assertEquals(set1, set2);
     }
 }
